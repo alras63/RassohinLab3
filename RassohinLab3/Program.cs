987cf08b48d0c84b1925e2f91dc42a73b13c1b32
ural1 <ural1@DESKTOP-EKUFV44>
2019-10-03 11:01:39 +0400

    Добавьте файлы проекта.

---
+++ RassohinLab3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RassohinLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ПАЛИНДРОМ - Слово или фраза, которые одинаково читаются слева направо и справа налево
+            Console.WriteLine("Введите строку, а мы определим, является ли она палиндромом");
+            string s = Console.ReadLine();
+            string result = palindrom(s);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        //реализация рекурсии
+        public static string palindrom(string s)
+        {
+            if (s.Length == 1)
+            {
+                return "YES"; //если строка из одного символа, то она по умолчанию палиндром 
+            } else
+            {
+                if (s.Substring(0, 1).Equals(s.Substring(s.Length - 1))) //Извлекаем первый символ, сравниваем этот экземляр с последним
+                {
+                    if (s.Length == 2)
+                    {
+                        return "YES"; //если символы равны и длина строки равна двум, то это успех
+                    }
+
+                    //если длина строки больше, пользуемся рекурсией
+                    //передаем новую строку, исключив из текущий первый и последний символ
+                    return palindrom(s.Substring(1, s.Length - 2));
+                }
+                else
+                {
+                    return "NO";
+                }
+            }
+        }
+    }
+}
---
+++ RassohinLab3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("RassohinLab3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RassohinLab3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5bebe4d7-b732-4f80-ba3e-02cb8e37d2f6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
